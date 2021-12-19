10418810f8eb6ff39f9e046a5d5da95c220f3057
johntaylorpa <johntaylorpa@outlook.com>
2021-12-18 22:26:11 -0500

    editing code

--- login.py
+++ login.py
@@ -5,26 +5,28 @@ from streamlit.type_util import data_frame_to_bytes
 import pandas as pd
 from pandas import DataFrame
 
+hide_menu_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer: {visibility: hidden;}
+</style>
+
+"""
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
 myusr = st.secrets["myusr"]
 mypwd = st.secrets["mypwd"]
 
 client = MongoClient(f"mongodb+srv://{myusr}:{mypwd}@cluster0.vkduz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-# client = MongoClient("mongodb+srv://john:12345@cluster0.vkduz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.get_database("test")
 users = db.get_collection("users")
-
-#THIS WORKS BELOW
-# data = db.users.count_documents({'username':'Gina', 'password':'GinaPHP1'})  #registers a 1 if correct info and a 0 if not
-# print(data)
-#THIS WORKS ABOVE
 
 #%%
 def login_user(username, password):
     data = db.users.count_documents({'username':username, 'password':password})
-    # data = db.users.find({"username":username, "password":password})
     return data
-
-
 
 def app_page():
     st.title("App Page")
